a9d51d569cd38e5b5ec83c4b0f6c165f297cc21f
TheOrangeDot <theorangedottwitch@gmail.com>
2021-12-09 23:40:04 +0900

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+class Polygon {
+  constructor(side) {
+    this.side = side;
+  }
+
+  get countSides() {
+    return this.side.length;
+  }
+
+  get perimeter() {
+    return this.side.reduce((total, side) => {
+      return total + side;
+    }, 0);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    let sorted = this.side.sort();
+    let a = sorted[0] * sorted[0];
+    let b = sorted[1] * sorted[1];
+    let c = sorted[2] * sorted[2];
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    return a + b > c;
+  }
+}
+
+class Square extends Polygon {
+  get area() {
+    return this.side[0] * this.side[0];
+  }
+
+  get isValid() {
+    for (let i = 1; i < this.side.length; i++) {
+      return this.side[0] === this.side[i];
+    }
+  }
+}
+
+let square = new Square([5, 5, 5, 5]);
+let triangle = new Triangle([5, 5, 5]);
+let fakeTriangle = new Triangle([15, 10, 1]);
+
+// console.log(square);
+// console.log(square.countSides);
+// console.log(square.perimeter);
+console.log(square.getArea);
+// console.log(square.isValid);
+
+// console.log(triangle.isValid);
+// console.log(fakeTriangle.isValid);
